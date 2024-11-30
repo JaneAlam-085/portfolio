c9ae22d87c8f8d1b47de9166616a9ce3e9495be5
JaneAlam-085 <janealam6069@gmail.com>
2024-11-30 20:47:22 +0530

    commit jane

--- src/App.js
+++ src/App.js
@@ -8,29 +8,28 @@ import Navebar from "./Component/Navebar/Navebar";
 import Projects from "./Component/Projects/Projects";
 import Skills from "./Component/Skills/Skills";
 
-
 function App() {
   const images = [
     "https://drive.google.com/open?id=1VxlGWvAqpRB4yRIj2NJL4HkXn4QDYNha&usp=drive_fs",
     "https://drive.google.com/file/d/1Z30XmciJQlwad-bpRmRg3twPT3v5Z94H/view?pli=1",
     "https://drive.google.com/file/d/1p9_AYcTSUasgB-KO2ftiN0bGH_TltEol/view?pli=1",
     "https://drive.google.com/file/d/18nUzOrCwc4dlO7jodFx9iLCMzE_rV62k/view?pli=1",
-    ""
+    "",
   ];
 
   return (
     <>
-    <div className="bg-[#171d32] h-auto w-full overflow-hidden">
-      <Navebar/>
-      <Home />
-      <About />
-      <Skills />
-      <Education />
-      <Markofall />
-      <Cirtificate images={images} autoSlide={true} interval={2000} />
-      <Projects />
-      <Footer />
-    </div>
+      <div className="bg-[#171d32] h-auto w-full overflow-hidden">
+        <Navebar />
+        <Home />
+        <About />
+        <Skills />
+        <Education />
+        <Markofall />
+        <Cirtificate images={images} autoSlide={true} interval={2000} />
+        <Projects />
+        <Footer />
+      </div>
     </>
   );
 }
